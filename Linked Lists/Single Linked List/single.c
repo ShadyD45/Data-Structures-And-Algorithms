b8d5a482b040ea9b559feef224529f7eadd29946
Shubham Dhumal <54709753+ShadyD45@users.noreply.github.com>
2020-12-28 12:01:05 +0530

    added main driver function

--- Linked Lists/Single Linked List/single.c
+++ Linked Lists/Single Linked List/single.c
@@ -27,53 +27,53 @@ int main()
 		switch(choice)
 		{
 			case 1:	printf("\nHow many elements you want:-->");
-					scanf("%d",&n);
+				scanf("%d",&n);
 					
-					printf("\nEnter the elements:-->");
-					for(i=0;i<n;i++)
-					{
-						scanf("%d",&m);
-						CreateList(m);		
-					}
-					break;
+				printf("\nEnter the elements:-->");
+				for(i=0;i<n;i++)
+				{
+					scanf("%d",&m);
+					CreateList(m);		
+				}
+				break;
 
 			case 2:	printf("\nEnter The Element To Add:-->");
-					scanf("%d",&m);
-					AddAtBeg(m);	
-					break;
+				scanf("%d",&m);
+				AddAtBeg(m);	
+				break;
 
 			case 3:	printf("\nEnter The Element To Add:-->");
-					scanf("%d",&m);
+				scanf("%d",&m);
 					
-					printf("\nEnter position:-->");
-					scanf("%d",&position);
-					InsertAt(m,position);	
-					break;
+				printf("\nEnter position:-->");
+				scanf("%d",&position);
+				InsertAt(m,position);	
+				break;
 
 			case 4:	if(start==NULL)
-					{
-						printf("\nList is empty...");
-					}	
+				{
+					printf("\nList is empty...");
+				}	
 							
-					printf("\nEnter element to delete:-->");
-					scanf("%d",&m);
+				printf("\nEnter element to delete:-->");
+				scanf("%d",&m);
 
-					del(m);	
-					break;
+				del(m);	
+				break;
 
 			case 5:	display();	 
-					break;
+				break;
 			
 			case 6: count();	
-					break;
+				break;
 
 			case 7: reverse();	
-					break;
+				break;
 
 			case 8: printf("\nEnter Element To Search:-->");
-					scanf("%d",&m);
-					search(m);	
-					break;
+				scanf("%d",&m);
+				search(m);	
+				break;
 
 			case 9: exit(1);
 
