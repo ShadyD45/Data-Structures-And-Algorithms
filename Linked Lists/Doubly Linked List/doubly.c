caace36c9e1295413d87af87d3a71b41f8de6fc3
Shubham Dhumal <54709753+ShadyD45@users.noreply.github.com>
2020-12-28 18:05:23 +0530

    Update doubly.c

--- Linked Lists/Doubly Linked List/doubly.c
+++ Linked Lists/Doubly Linked List/doubly.c
@@ -3,6 +3,7 @@
 #include<malloc.h>
 #include"change.h"
 #include"view.h"
+
 int main()
 {
 	int choice,n,m,p,i;
@@ -31,33 +32,33 @@ int main()
 				for(i=0;i<n;i++)
 				{
 					scanf("%d",&m);
-					CreateList(m);		
+					create_list(m);		//calling create list function
 				}
 				break;
 
 			case 2:	printf("\nEnter The Element To Add:-->");
 				scanf("%d",&m);
-				AddAtBeg(m);		
+				addatbeg(m);	//calling addatbeg function	
 				break;
 
 			case 3:	printf("\nEnter The Element To Add and Position:-->");
 				scanf("%d %d",&m,&p);
-				InsertAt(m,p);	
+				addafter(m,p);	//calling addafter function
 				break;
 
 			case 4:	printf("\nEnter element to delete:-->");
 				scanf("%d",&m);
 
-				del(m);	
+				del(m);	//calling delete function
 				break;
 
-			case 5:	display();	
+			case 5:	display();	//calling display function 
 				break;
 			
-			case 6: count();	
+			case 6: count();	//calling count function
 				break;
 
-			case 7: reverse();	
+			case 7: reverse();	//calling reverse function
 				break;
 
 			case 8: exit(1);
@@ -69,3 +70,158 @@ int main()
 	}
 }
 
+void create_list(int no)
+{
+	struct node *temp,*q;
+	
+	temp=malloc(sizeof(struct node *));
+	temp->data=no;
+	temp->next=NULL;
+	
+	if(start==NULL)
+	{
+		temp->prev=NULL;
+		start=temp;
+		last=start;
+	}
+	else
+	{
+		last->next=temp;
+		temp->prev=last;
+		last = temp;
+	}
+}//End of create list
+
+void addatbeg(int no)
+{
+	struct node *temp;
+	temp=malloc(sizeof(struct node *));
+	
+	temp->data=no;
+	temp->prev=NULL;
+	temp->next=start;
+	start->prev=temp;
+	start=temp;
+	printf("\nElement %d Added at beginning\n\n",no);
+}//End of add at beginning
+
+void addafter(int no,int p)
+{
+	struct node *temp,*q;
+	int i;
+	q=start;
+
+	for(i=1;i<p-1;i++)
+	{
+		q=q->next;
+		if(q->next==NULL)
+		{
+			printf("\nPlease enter valid position:(\n\n");
+			return;		
+		}
+	}
+	
+	temp=malloc(sizeof(struct node *));
+	temp->data=no;
+	temp->next=q->next;
+	temp->prev=q;
+	q->next=temp;	
+	printf("\nElement %d Added at position %d\n\n",no,p);
+}//End of Add After
+
+void del(int no)
+{
+	struct node *temp,*q;
+	
+	if(start->data==no)
+	{
+		temp=start;
+		start=start->next; //first element deleted
+		start->prev=NULL;
+		free(temp);
+		printf("\nElement %d deleted\n\n",no);
+		return;	
+	}
+	q=start;
+
+	while(q->next->next!=NULL)
+	{
+		if(q->next->data==no)	//To delete element in between
+		{
+			temp=q->next;
+			q->next=temp->next;
+			temp->next->prev=q;
+			free(temp);
+			printf("\nElement %d deleted\n\n",no);
+			return;
+		}
+		
+		q=q->next;	
+	}
+
+	if(q->next->data==no)	//To delete last element
+	{
+		temp=q->next;
+		free(temp);
+		q->next=NULL;
+		last = q;
+		printf("\nElement %d deleted\n\n",no);
+		return;
+	}
+	
+	printf("\nElement not %d found\n\n",no);
+}//End of delete
+
+void reverse()
+{
+	struct node *p1,*p2;
+	p1=start;
+	last=start;
+	p2=p1->next;
+	p1->next=NULL;
+	p1->prev=p2;
+
+	while(p2!=NULL)
+	{
+		p2->prev=p2->next;
+		p2->next=p1;
+		p1=p2;
+		p2=p2->prev;
+	}
+	start=p1;
+	printf("\nList Reversed\n\n");	
+}//End of reverse
+
+void display()
+{
+	struct node *q;
+	
+	if(start==NULL)
+	{
+		printf("\nList is empty");
+		return;
+	}
+	q=start;
+	
+	printf("\nList is:-->");
+	while(q!=NULL)
+	{
+		printf("\t %4d",q->data);
+		q=q->next;
+	}
+	printf("\n\n");
+}//End od Diplay
+
+void count()
+{
+	struct node *q=start;
+	int cnt=0;
+
+	while(q!=NULL)
+	{
+		cnt++;
+		q=q->next;
+	}
+
+	printf("\nThere are %d no. of elemenets in the list\n\n",cnt);
+}
