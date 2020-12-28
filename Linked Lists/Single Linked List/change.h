f2eb4bc279b643cbb50ada63a71bf4b91921f81a
Shubham Dhumal <54709753+ShadyD45@users.noreply.github.com>
2020-12-28 11:47:04 +0530

    added functions to create list and delete
    Includes functions to change the list

---
+++ Linked Lists/Single Linked List/change.h
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<malloc.h>
+#include<stdlib.h>
+
+struct node
+{
+	int info;
+	struct node *link;
+}*start=NULL,*last=NULL;
+
+
+void CreateList(int);
+void AddAtBeg(int);
+void InsertAt(int,int);
+void del(int);
+void reverse();
+
+void CreateList(int data)
+{
+	struct node *ptr,*temp;
+	temp=malloc(sizeof(struct node *));
+	temp->info=data;
+	temp->link=NULL;	//if list only has single element
+
+	if(start==NULL)
+	{
+		start = temp;
+		last  = start;
+	}
+	else
+	{
+		last->link = temp;
+		last = temp;
+	}
+}//End Of Create List function
+
+void del(int data)
+{
+	struct node *temp,*ptr;
+	
+	if(start == NULL)
+	{
+		printf("List is empty!!\n");
+		return;
+	}		
+	
+	if(start->info == data) /* Deletion of first node*/
+	{
+		temp = start;
+		start = start->link;
+		free(temp);
+		return;
+	}
+	
+	ptr = start;	/* Deletion in between or at end */
+	while(ptr->link != NULL)
+	{
+		if(ptr->link->info == data)
+		{
+			temp = ptr->link;
+			
+			if(temp->link == NULL)
+				last = ptr;
+				
+			ptr->link = temp->link;
+			free(temp);
+			return;
+		}
+		ptr = ptr->link;
+	}
+	printf("Element %d not found\n",data);	
+}//End of delete function
