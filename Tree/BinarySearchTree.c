b6d6d1a436e538fa096f450233023a8c9385177a
Shubham Dhumal <54709753+ShadyD45@users.noreply.github.com>
2021-03-24 14:23:24 +0530

    added function to mirror the binary tree
    also fixed some bugs and typos

--- Tree/BinarySearchTree.c
+++ Tree/BinarySearchTree.c
@@ -11,11 +11,12 @@ struct node
 	struct node *right;
 }*root=NULL;
 
-void CreateBST(int);
+void InsertInBST(int);
+void Mirror(struct node*);
 void PreorderTraversal(struct node*);
 void InorderTraversal(struct node*);
 void PostorderTraversal(struct node*);
-int GetHeightOfTree(struct node*);
+int  GetHeightOfTree(struct node*);
 
 int main()
 {
@@ -25,32 +26,34 @@ int main()
 	while(1)
 	{
 		printf("\n\n1) Create Tree\n2) Traverse Using Preorder");
-		printf("\n3) Traverse Using Inorder\n4) Traverse Using Postorder\n5) Get Height Of Tree\n6) Exit\nChoice: ");
+		printf("\n3) Traverse Using Inorder\n4) Traverse Using Postorder\n5) Get Height Of Tree\n6)Mirror\n7) Exit\nChoice: ");
 		scanf("%d",&iChoice);
 		
 		switch(iChoice)
 		{
 			case 1: printf("Enter Number of Nodes: ");
-				scanf("%d",&iNodeCnt);
-				printf("Enter the elements: ");
-				for(i = 0; i < iNodeCnt; ++i)
-				{
-					scanf("%d",&iData);
-					CreateBST(iData);
-				}
-				break;
+					scanf("%d",&iNodeCnt);
+					printf("Enter the elements: ");
+					for(i = 0; i < iNodeCnt; ++i)
+					{
+						scanf("%d",&iData);
+						InsertInBST(iData);
+					}
+					break;
 			case 2: PreorderTraversal(root);
-				printf("\n");
-				break;
+					printf("\n");
+					break;
 			case 3: InorderTraversal(root);
-				printf("\n");
-				break;		
+					printf("\n");
+					break;		
 			case 4: PostorderTraversal(root);
-				printf("\n");
-				break;				
+					printf("\n");
+					break;	
 			case 5: printf("Height is: %d\n",GetHeightOfTree(root));
-				break;					
-			case 6: exit(1);
+					break;
+			case 6: Mirror(root);	
+					break;				
+			case 7: exit(1);
 				
 			default: printf("\nWrong Choice!! Please enter correct option");
 		}
@@ -58,7 +61,7 @@ int main()
 	return 0;
 }
 
-void CreateBST(int iData)
+void InsertInBST(int iData)
 {
 	struct node *temp, *ptr;
 	temp = (struct node *)malloc(sizeof(struct node));
@@ -99,34 +102,59 @@ void CreateBST(int iData)
 	}
 }
 
+
+void Mirror(struct node* node) 
+{
+    if (node == NULL) 
+    { 
+       return; 
+    }
+    else
+    {
+        struct node* temp;
+          
+        /* do the subtrees */
+        Mirror(node->left);
+        Mirror(node->right);
+      
+        /* swap the pointers in this node */
+        temp     = node->left;
+        node->left = node->right;
+        node->right = temp;
+    }
+}
+
 void PreorderTraversal(struct node *ptr)
 {
-	if(ptr != NULL)
+	if(ptr == NULL)
 	{
-		printf("%d  ",ptr->iInfo);
-		PreorderTraversal(ptr->left);
-		PreorderTraversal(ptr->right);
+		return;
 	}
+	printf("%d  ",ptr->iInfo);
+	PreorderTraversal(ptr->left);
+	PreorderTraversal(ptr->right);
 }
 
 void InorderTraversal(struct node *ptr)
 {
-	if(ptr != NULL)
+	if(ptr == NULL)
 	{
-		PreorderTraversal(ptr->left);
-		printf("%d  ", ptr->iInfo);
-		PreorderTraversal(ptr->right);
+		return;
 	}
+	InorderTraversal(ptr->left);
+	printf("%d  ", ptr->iInfo);
+	InorderTraversal(ptr->right);
 }
 
 void PostorderTraversal(struct node *ptr)
 {
-	if(ptr != NULL)
+	if(ptr == NULL)
 	{
-		PreorderTraversal(ptr->left);
-		PreorderTraversal(ptr->right);
-		printf("%d  ",ptr->iInfo);
+		return;
 	}
+	PostorderTraversal(ptr->left);
+	PostorderTraversal(ptr->right);
+	printf("%d  ",ptr->iInfo);
 }
 
 int GetHeightOfTree(struct node *root)
